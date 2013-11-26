7ae909779f3f6f5023261b0122a5e7e37ed351b5
Jonian <jonian@hardpixel.net>
2013-11-26 02:35:08 +0200

    Removed Cuztom Helper. Combined Javascripts.

--- .forge/build/javascripts/theme.js
+++
@@ -1,5 +0,0 @@
-//= require 'main'
-
-jQuery(document).ready(function($) {
-	$('.gallery-item a').swipebox();
-});
--- source/functions/post_types.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$slider = new Cuztom_Post_Type( 'Slider', array(
-		'supports'				=> array( 'title' ),
-		'show_in_nav_menus'		=> false
-	) );
